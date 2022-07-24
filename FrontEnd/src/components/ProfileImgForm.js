64df0221085dcd09679b05cad088c2538f4f4937
wonwoo <lww0407@naver.com>
2022-07-24 19:18:20 +0900

    WW-feature: Input Fixed & Profile Image Uploader

--- FrontEnd/src/components/ProfileImgForm.js
+++ FrontEnd/src/components/ProfileImgForm.js
@@ -1,19 +1,88 @@
-import React, { Fragment, useState } from 'react';
-import styled from 'styled-components';
+import React, { Fragment, useState } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  position: ${(props) =>
+  props.imagePosition ? props.imagePosition : "relative"};
+  width: ${(props) => (props.imageWidth ? props.imageWidth : 150 + "px")};
+  height: ${(props) => (props.imageWidth ? props.imageWidth : 150 + "px")};
+  border-radius: ${(props) => (props.imageWidth ? props.br : 5 + "px")};
+  background-color: ${(props) => (props.bgColor ? props.bgColor : "")};
+  margin-bottom: ${(props) => (props.mb ? props.mb + "px" : 0)};
+  margin-top: ${(props) => (props.mt ? props.mt + "px" : 0)};
+  margin-right: ${(props) => (props.mr ? props.mr + "px" : 0)};
+  margin-left: ${(props) => (props.ml ? props.ml + "px" : 0)};
+  background: ${(props) => props.imagePreview ? `url("${props.imagePreview}") no-repeat center/cover`: ""};
+  cursor: ${props => props.cursor ? props.cursor : 'default'};
+  border: ${props => props.border ? props.border : ''};
+
+  & div input {
+    display: none;
+  }
+
+  & div label {
+    visibility: ${(props) => (props.imagePreview ? "hidden" : "visible")};
+  }
+  & div label img {
+    cursor: pointer;
+  }
+`;
 
 const ProfileImgForm = () => {
-  const [image, setImage] = useState("https://via.placeholder.com/150");
-  const ProfileImgDiv = styled.div`
-    width: 150px;
-    height: 150px;
-    border-radius: 5px;
-    
-    
-  `
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState(false);
+
+  const ImageUploader = (e) => {
+    const imageSelected = e.target.files[0];
+    const types = ["image/png", "image/jpeg", "image/jpg"];
+    if (imageSelected && types.includes(imageSelected.type)) {
+      let ImgReader = new FileReader();
+      ImgReader.readAsDataURL(imageSelected);
+      ImgReader.onloadend = (e) => {
+        setImage(e.target.result);
+      };
+    } else {
+      setError(true);
+    }
+  };
+  const ImageRemover = () => {
+    setImage((state) => state ? null : null)
+  }
   return (
-    <ProfileImgDiv>
-      
-    </ProfileImgDiv>
+    <Div>
+      <Div
+        imageWidth="170px"
+        imageHeight="170px"
+        imagePreview={image}
+        br="13px"
+      >
+        {error && <p>File not supported</p>}
+        <Div className="d-flex justify-content-center align-items-center">
+          <div>
+            <label htmlFor="fileUpload">
+              <img
+                src="https://icons-for-free.com/download-icon-profile+profile+page+user+icon-1320186864367220794_512.png"
+                alt=""
+              />
+            </label>
+            <input type="file" id="fileUpload" onChange={ImageUploader} />
+          </div>
+          {image && (
+            <Div
+              imageWidth="40px"
+              imageHeight="40px"
+              br="50px"
+              imagePreview="https://pngroyale.com/wp-content/uploads/2022/05/Red-Minus-Symbol-Transparent-Image.png"
+              imagePosition="absolute"
+              ml={170}
+              mt={170}
+              onClick={ImageRemover}
+              cursor="pointer"
+            ></Div>
+          )}
+        </Div>
+      </Div>
+    </Div>
   );
 };
 
--- FrontEnd/src/pages/SignupPage.js
+++ FrontEnd/src/pages/SignupPage.js
@@ -3,6 +3,30 @@ import AuthWrapper from "../layout-wrapper/AuthWrapper";
 import InputComponent from "../components/InputComponent";
 import styled from "styled-components";
 import ButtonComponent from "../components/ButtonComponent";
+import ProfileImgForm from "../components/ProfileImgForm";
+
+// 로고 사진 대신 사용하는 임시 Div 스타일
+const DivStyle = styled.div`
+  width: 462px;
+  height: 100%;
+  background-color: blue;
+  border-bottom-left-radius: 5px;
+  border-top-left-radius: 5px;
+`;
+
+// 회원가입 페이지에서 사용할 div 스타일
+const Div = styled.div`
+  margin-bottom: ${(props) => (props.mb ? props.mb + "px" : 0)};
+  margin-top: ${(props) => (props.mt ? props.mt + "px" : 0)};
+  margin-right: ${(props) => (props.mr ? props.mr + "px" : 0)};
+  margin-left: ${(props) => (props.ml ? props.ml + "px" : 0)};
+`;
+
+const FormContainer = styled.form`
+  width: 830px;
+  height: 100%;
+`;
+
 
 const SignupPage = () => {
   const classList = ["d-flex"];
@@ -11,114 +35,93 @@ const SignupPage = () => {
     console.log(e);
   };
 
-  const DivStyle = styled.div`
-    width: 462px;
-    height: 100%;
-    background-color: blue;
-    border-bottom-left-radius: 5px;
-    border-top-left-radius: 5px;
-  `;
-
-  const DivButton = styled.div`
-    margin-bottom: 76px;
-  `;
-
-  const DivLarge = styled.div`
-    margin-top: 80px;
-    margin-bottom: 50px;
-    margin-right: 30px;
-  `;
-  const FormContainer = styled.form`
-    width: 830px;
-    height: 100%;
-  `;
 
   return (
     <AuthWrapper classList={classList}>
       <DivStyle className="main-theme-bg"></DivStyle>
-      <FormContainer onSubmit={SubmitHandler}>
-        <DivLarge>
+      <FormContainer onSubmit={SubmitHandler} autocomplete="off">
+        <Div mt={100}>
           <div className="d-flex justify-content-around mb-1">
-            <div>
-              <div className="ms-5 mb-5">
-                <img src="https://via.placeholder.com/150" alt="" />
-              </div>
-              <DivButton>
+            <Div>
+              <Div ml={100} mb={50}>
+                <ProfileImgForm></ProfileImgForm>
+              </Div>
+              <Div mb={65}>
                 <InputComponent
                   inputLabel="이름(성)"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div mb={65}>
                 <InputComponent
                   inputLabel="이름"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div mb={65}>
                 <InputComponent
-                  inputLabel="이름"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputLabel="생년월일"
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-            </div>
+              </Div>
+            </Div>
 
-            <div>
-              <DivButton>
+            <Div mt={20} mr={25}>
+              <Div mb={65}>
                 <InputComponent
                   inputLabel="이메일"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div mb={65}>
                 <InputComponent
                   inputLabel="비밀번호"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div mb={65}>
                 <InputComponent
                   inputLabel="비밀번호 확인"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div>
                 <InputComponent
                   inputLabel="휴대전화"
-                  inputHeight={25}
-                  inputWidth={330}
+                  inputHeight="25px"
+                  inputWidth="330px"
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-              <DivButton>
+              </Div>
+              <Div>
                 <InputComponent
                   inputLabel="성별"
-                  inputHeight={25}
+                  inputHeight="25px"
                   inputWidth={50}
                   inputValid={true}
                   labelTop={1.3}
                 ></InputComponent>
-              </DivButton>
-            </div>
+              </Div>
+            </Div>
           </div>
 
           <div className="d-flex justify-content-center">
@@ -126,13 +129,14 @@ const SignupPage = () => {
               <ButtonComponent
                 inputWidth={358}
                 buttonHeigh={50}
+                buttonText="회원 가입"
               ></ButtonComponent>
               <div className="d-flex justify-content-center mt-3">
                 다른 아이디로 로그인 하기
               </div>
             </div>
           </div>
-        </DivLarge>
+        </Div>
       </FormContainer>
     </AuthWrapper>
   );
