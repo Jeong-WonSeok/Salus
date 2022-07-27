f135e6b32ced0931fa6eb884ed68cfd197777d62
ji01217 <remi1217@naver.com>
2022-07-27 23:56:36 +0900

    JY-feature: Exercise List

---
+++ FrontEnd/src/components/Kiosk/Exercise.js
@@ -0,0 +1,3 @@
+const Exercise = () => {};
+
+export default Exercise;
--- FrontEnd/src/pages/MuscleTest.js
+++ FrontEnd/src/pages/MuscleTest.js
@@ -1,10 +1,16 @@
 import MuscleMan from '../components/MuscleMan';
 import MuscleWoman from '../components/MuscleWoman';
+import ExerciseList from '../components/Kiosk/ExerciseList';
 
 const MuscleTest = () => {
   const user = { isMale: false, muscles: ['traps', 'shoulders'] };
 
-  return <div>{user.isMale ? <MuscleMan muscles={user.muscles} /> : <MuscleWoman muscles={user.muscles} />}</div>;
+  return (
+    <div>
+      {user.isMale ? <MuscleMan muscles={user.muscles} /> : <MuscleWoman muscles={user.muscles} />}
+      <ExerciseList title="숄더프레스" volume="1000" reps="512" />
+    </div>
+  );
 };
 
 export default MuscleTest;
