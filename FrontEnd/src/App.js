e08bd3be90a80424e6e43cdd60c8857ffe2a1932
WONWOOLEE <lww0407@naver.com>
2022-07-27 14:05:38 +0900

    WW-feature: Rount Chart Start

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import CalendarPage from "./pages/CalendarPage";
+import KioskMainPage from './pages/KioskMainPage';
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/kiosk" element={<KioskMainPage/>}/>
       </Routes>
     </div>
   );
