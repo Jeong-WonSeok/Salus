153628932962bcddd3ccef60bfd6ead1872d3a47
wonseok <jws4858@gmail.com>
2022-07-28 03:48:43 +0900

    BE-dev WS-feature:Jira[#227][Google Login/Logout]

---
+++ BackEnd/routers/auth.js
@@ -0,0 +1,28 @@
+var express  = require('express');
+var router   = express.Router();
+var passport = require('../config/passport');
+
+router.get('/login', function(req,res){
+    res.render('auth/login');
+});
+
+router.get('/logout', function(req, res, next) {
+    req.logOut();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+router.get('/google',
+    passport.authenticate('google', { scope: ['profile'], prompt: 'select_account'})
+);
+
+router.get('/google/callback',
+    passport.authenticate('google'), authSuccess
+);
+
+function authSuccess(req, res) {
+    
+    res.redirect('/');
+}
+
+module.exports = router;
