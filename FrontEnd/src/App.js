74742d8957fdca16c4d595b893ee2ed84262295a
황상윤 <s_infinite@naver.com>
2022-08-01 03:40:54 +0900

    SY-feature: 임시

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,16 +1,16 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import KioskMainPage from './pages/KioskMainPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import MainPage from './pages/MainPage';
-import CalendarPage from './pages/CalendarPage';
-import FindAccountPage from './pages/FindAccountPage';
-import KioskLoginPage from './pages/KioskLoginPage';
-import RecordPage from './pages/RecordPage';
-import AdminPage from './pages/AdminPage';
-import EquipmentListPage from './pages/EquipmentListPage'
+import { Route, Routes, Navigate } from "react-router-dom";
+import KioskMainPage from "./pages/KioskMainPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import MainPage from "./pages/MainPage";
+import CalendarPage from "./pages/CalendarPage";
+import FindAccountPage from "./pages/FindAccountPage";
+import KioskLoginPage from "./pages/KioskLoginPage";
+import RecordPage from "./pages/RecordPage";
+import AdminPage from "./pages/AdminPage";
+import EquipmentListPage from "./pages/EquipmentListPage";
 
-import NavSideBar from './components/Nav&Side/NavSideBar';
+import NavSideBar from "./components/Nav&Side/NavSideBar";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
         <Route element={<NavSideBar />}>
           <Route path="/main" element={<MainPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Route>
         <Route path="/" element={<Navigate push to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
@@ -26,9 +27,9 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/kiosk" element={<KioskMainPage />} />
         <Route path="/kiosk/login" element={<KioskLoginPage />} />
-        <Route path="/record" element={<RecordPage/>}/>
-        <Route path="/admin" element={<AdminPage/>} />
-        <Route path="/equipment-list" element={<EquipmentListPage/>}/>
+        <Route path="/record" element={<RecordPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/equipment-list" element={<EquipmentListPage />} />
       </Routes>
     </div>
   );
--- FrontEnd/src/pages/AdminPage.js
+++ FrontEnd/src/pages/AdminPage.js
@@ -1,11 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import AdminComponent from "../components/Admin/AdminComponent";
 
-import React from 'react';
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Title = styled.div`
+  width: 85%;
+  height: 15%;
+  font-size: 34px;
+  line-height: 36px;
+  font-weight: 600;
+  text-align: center;
+  margin: 0 auto;
+
+  &div {
+    margin-top: 2rem;
+  }
+`;
 
 const AdminPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Div>
+      <Title>Admin Page</Title>
+
+      <AdminComponent />
+    </Div>
   );
 };
 
