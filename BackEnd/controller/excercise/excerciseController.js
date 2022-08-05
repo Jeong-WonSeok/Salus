d40feb8ac358125d5bf6aec4ad945ff090d59e6d
박성배 <sbpark0402@gmail.com>
2022-08-05 14:43:23 +0900

    Delete excerciseController.js

--- BackEnd/controller/excercise/excerciseController.js
+++
@@ -1,6 +0,0 @@
-/** 컨트롤러 */
-const ExcerciseModel = require("../../model/excercise/excerciseModel");
-const searchExcerciseData = ExcerciseModel.ExcerciseData;
-module.exports = {
-  searchExcerciseData,
-};
