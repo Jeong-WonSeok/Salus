c5fcfc4aff58dc6b000ddf5d9d46de22c5891bc6
박성배 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-08-09 11:53:33 +0900

    BE-dev BE-park: 소켓 통신관련 API 추가 구현

--- BackEnd/salus.sql
+++ BackEnd/salus.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `rfidKey` bigint(20) NOT NULL,
   `dailyExcerciseStart` datetime DEFAULT NULL,
   `dailyExcerciseEnd` datetime DEFAULT NULL,
-  `attendanceCheck` tinyint(4) unsigned zerofill DEFAULT NULL,
+  `attendanceCheck` tinyint(1) unsigned zerofill DEFAULT NULL,
   `excerciseDay` date DEFAULT NULL,
   `firstRegist` datetime DEFAULT current_timestamp(),
   `updateRegist` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -30,13 +30,14 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   CONSTRAINT `fk_rfidkey` FOREIGN KEY (`rfidKey`) REFERENCES `user` (`rfidKey`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 테이블 데이터 salus.attendance:~4 rows (대략적) 내보내기
+-- 테이블 데이터 salus.attendance:~5 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
 INSERT INTO `attendance` (`rfidKey`, `dailyExcerciseStart`, `dailyExcerciseEnd`, `attendanceCheck`, `excerciseDay`, `firstRegist`, `updateRegist`) VALUES
-	(12341234, '2022-08-04 14:00:00', '2022-08-08 21:47:17', 0000, '2022-08-04', '2022-08-04 14:40:23', '2022-08-08 21:47:17'),
-	(12341234, '2022-08-05 14:00:00', '2022-08-08 21:47:17', 0000, '2022-08-05', '2022-08-05 13:53:46', '2022-08-08 21:47:17'),
-	(11111111, '2022-08-05 14:10:43', '2022-08-05 14:11:17', 0000, '2022-08-05', '2022-08-05 14:10:43', '2022-08-05 14:11:17'),
-	(12341234, '2022-08-08 11:00:00', '2022-08-08 21:47:17', 0000, '2022-08-08', '2022-08-08 09:48:20', '2022-08-08 21:47:17');
+	(12341234, '2022-08-04 14:00:00', '2022-08-09 01:55:35', 0, '2022-08-04', '2022-08-04 14:40:23', '2022-08-09 01:55:35'),
+	(12341234, '2022-08-05 14:00:00', '2022-08-09 01:55:35', 0, '2022-08-05', '2022-08-05 13:53:46', '2022-08-09 01:55:35'),
+	(11111111, '2022-08-05 14:10:43', '2022-08-05 14:11:17', 0, '2022-08-05', '2022-08-05 14:10:43', '2022-08-05 14:11:17'),
+	(12341234, '2022-08-08 11:00:00', '2022-08-09 01:55:35', 0, '2022-08-08', '2022-08-08 09:48:20', '2022-08-09 01:55:35'),
+	(12341234, '2022-08-09 01:54:18', '2022-08-09 01:55:35', 0, '2022-08-09', '2022-08-09 01:54:18', '2022-08-09 01:55:35');
 /*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
 
 -- 테이블 salus.board 구조 내보내기
@@ -101,6 +102,7 @@ CREATE TABLE IF NOT EXISTS `fitness_equipment` (
   `equipmentDescription` text DEFAULT NULL,
   `firstRegist` datetime NOT NULL DEFAULT current_timestamp(),
   `updateRegist` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `isStarted` tinyint(4) DEFAULT 0,
   PRIMARY KEY (`equipmentId`) USING BTREE,
   KEY `equipmentCategory` (`equipmentCategory`) USING BTREE,
   KEY `equipmentName` (`equipmentName`) USING BTREE,
@@ -111,33 +113,33 @@ CREATE TABLE IF NOT EXISTS `fitness_equipment` (
 
 -- 테이블 데이터 salus.fitness_equipment:~26 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `fitness_equipment` DISABLE KEYS */;
-INSERT INTO `fitness_equipment` (`equipmentId`, `equipmentName`, `equipmentEnglishName`, `equipmentCategory`, `gymid`, `equipmentImage`, `equipmentStimulate`, `equipmentEnglishStimulate`, `equipmentDescription`, `firstRegist`, `updateRegist`) VALUES
-	(1, '런닝머신', 'Treadmill', '유산소', 1, NULL, '', '', '야외에서 달리는 것처럼 제자리뛰기를 할 수 있게 해준다.', '2022-07-27 17:24:52', '2022-07-27 17:24:52'),
-	(2, '로잉 머신', 'Rowing Machine', '전신', 1, NULL, '', '', '로잉 머신(Rowing machine)은 조정(스포츠) 선수들이 실내에서 운동 할 때 쓰는 기구이고 전신에 자극을 줄 수 있다는 것과 자비 없는 운동량으로 유명하다.', '2022-07-27 17:42:10', '2022-07-27 17:42:10'),
-	(3, '숄더프레스 머신', 'Sholder Press Machine', '상체', 1, NULL, '어깨', 'shoulders', '어깨 근육(삼각근)의 전면과 측면 부위의 근매스를 올리기 좋은 대표적인 운동입니다.', '2022-07-27 17:57:37', '2022-07-27 17:57:37'),
-	(4, '체스트 프레스 머신', 'Chest Press Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 22:59:58', '2022-07-27 22:59:58'),
-	(5, '버터플라이 머신', 'Butterfly Machine', '상체', 1, NULL, '가슴, 어깨, 복근', 'pecs, shoulders, obliques', '', '2022-07-27 23:01:33', '2022-07-27 23:01:33'),
-	(6, '펙덱 플라이 머신', 'Fly - Pec Dec Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 23:02:11', '2022-07-27 23:02:11'),
-	(7, '업도미널 머신', 'Abdominal Machine', '상체', 1, NULL, '복근', 'obliques', '', '2022-07-27 23:02:49', '2022-07-27 23:02:49'),
-	(8, '컬 머신', 'Curl Machine', '상체', 1, NULL, '이두근', 'biceps', '', '2022-07-27 23:03:36', '2022-07-27 23:03:36'),
-	(9, '리어델트 머신', 'Rear Delt Machine', '상체', 1, NULL, '어깨', 'shoulders', '', '2022-07-27 23:04:02', '2022-07-27 23:04:02'),
-	(10, '렛 풀다운 머신', 'Lat Pull Down Machine', '상체', 1, NULL, '광배근, 삼두근', 'lats, triceps', '', '2022-07-27 23:04:58', '2022-07-28 00:29:23'),
-	(11, '인클라인 벤치 프레스 머신', 'Incline Bench Press Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 23:05:38', '2022-07-27 23:07:19'),
-	(12, '레터럴 레이즈 머신', 'Lateral Raise Machine', '상체', 1, NULL, '어깨', 'shoulders', '', '2022-07-27 23:05:55', '2022-07-27 23:07:27'),
-	(13, '티 바 로우 머신', 'Tbar-row Machine', '상체', 1, NULL, '광배근', 'lats', '', '2022-07-27 23:06:16', '2022-07-27 23:06:16'),
-	(14, '시티드 로우 머신', 'Seated Row Machine', '상체', 1, NULL, '광배근, 승모근', 'lats, traps', '', '2022-07-27 23:06:30', '2022-07-28 00:27:04'),
-	(15, '어시스트 풀업 머신', 'Assisted Pullup Machine', '상체', 1, NULL, '광배근', 'lats', '', '2022-07-27 23:06:52', '2022-07-27 23:06:52'),
-	(16, '레그 컬 머신', 'Leg Curl Machine', '하체', 1, NULL, '허벅지, 종아리', 'quads, calves', '', '2022-07-27 23:08:13', '2022-07-28 00:22:46'),
-	(17, '레그 익스텐션 머신', 'Leg Extension Machine', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:09:36', '2022-07-27 23:10:17'),
-	(18, '힙 어덕터', 'Hip Adductor', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:10:07', '2022-07-27 23:12:01'),
-	(19, '핵 스쿼트 머신', 'Hack-squat Machine', '하체', 1, NULL, '허벅지, 햄스트링, 둔근', 'quads, hamstrings, glutes', '', '2022-07-27 23:11:59', '2022-07-27 23:11:59'),
-	(20, '브이 스쿼트 머신', 'V-squat Machine', '하체', 1, NULL, '허벅지, 둔근', 'quads, glutes', '', '2022-07-27 23:13:58', '2022-07-27 23:14:08'),
-	(21, '파워 레그프레스 머신', 'Power Leg Press Machine', '하체', 1, NULL, '허벅지, 둔근', 'quads, glutes', '', '2022-07-27 23:14:36', '2022-07-27 23:14:36'),
-	(22, '이너사이 머신', 'Inner Thigh Machine', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:15:02', '2022-07-27 23:15:02'),
-	(23, '라잉 레그 컬', 'Lying Leg Curl', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:15:26', '2022-07-27 23:15:26'),
-	(24, '시티드 레그 프레스 머신', 'Seated Leg Press Machine', '하체', 1, NULL, '허벅지, 엉덩이', 'quads, glutes', '', '2022-07-27 23:15:52', '2022-07-27 23:15:52'),
-	(25, '토탈 힙', 'Total Hip machine', '하체', 1, NULL, '허벅지, 엉덩이', 'quads, glutes', '', '2022-07-27 23:16:11', '2022-07-27 23:16:11'),
-	(26, '크런치 머신', 'Crunch machine', '상체', 1, NULL, '복근', 'obliques', '', '2022-07-27 23:17:36', '2022-07-27 23:17:36');
+INSERT INTO `fitness_equipment` (`equipmentId`, `equipmentName`, `equipmentEnglishName`, `equipmentCategory`, `gymid`, `equipmentImage`, `equipmentStimulate`, `equipmentEnglishStimulate`, `equipmentDescription`, `firstRegist`, `updateRegist`, `isStarted`) VALUES
+	(1, '런닝머신', 'Treadmill', '유산소', 1, NULL, '', '', '야외에서 달리는 것처럼 제자리뛰기를 할 수 있게 해준다.', '2022-07-27 17:24:52', '2022-08-09 11:49:54', 0),
+	(2, '로잉 머신', 'Rowing Machine', '전신', 1, NULL, '', '', '로잉 머신(Rowing machine)은 조정(스포츠) 선수들이 실내에서 운동 할 때 쓰는 기구이고 전신에 자극을 줄 수 있다는 것과 자비 없는 운동량으로 유명하다.', '2022-07-27 17:42:10', '2022-07-27 17:42:10', 0),
+	(3, '숄더프레스 머신', 'Sholder Press Machine', '상체', 1, NULL, '어깨', 'shoulders', '어깨 근육(삼각근)의 전면과 측면 부위의 근매스를 올리기 좋은 대표적인 운동입니다.', '2022-07-27 17:57:37', '2022-07-27 17:57:37', 0),
+	(4, '체스트 프레스 머신', 'Chest Press Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 22:59:58', '2022-07-27 22:59:58', 0),
+	(5, '버터플라이 머신', 'Butterfly Machine', '상체', 1, NULL, '가슴, 어깨, 복근', 'pecs, shoulders, obliques', '', '2022-07-27 23:01:33', '2022-07-27 23:01:33', 0),
+	(6, '펙덱 플라이 머신', 'Fly - Pec Dec Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 23:02:11', '2022-07-27 23:02:11', 0),
+	(7, '업도미널 머신', 'Abdominal Machine', '상체', 1, NULL, '복근', 'obliques', '', '2022-07-27 23:02:49', '2022-07-27 23:02:49', 0),
+	(8, '컬 머신', 'Curl Machine', '상체', 1, NULL, '이두근', 'biceps', '', '2022-07-27 23:03:36', '2022-07-27 23:03:36', 0),
+	(9, '리어델트 머신', 'Rear Delt Machine', '상체', 1, NULL, '어깨', 'shoulders', '', '2022-07-27 23:04:02', '2022-07-27 23:04:02', 0),
+	(10, '렛 풀다운 머신', 'Lat Pull Down Machine', '상체', 1, NULL, '광배근, 삼두근', 'lats, triceps', '', '2022-07-27 23:04:58', '2022-07-28 00:29:23', 0),
+	(11, '인클라인 벤치 프레스 머신', 'Incline Bench Press Machine', '상체', 1, NULL, '가슴', 'pecs', '', '2022-07-27 23:05:38', '2022-07-27 23:07:19', 0),
+	(12, '레터럴 레이즈 머신', 'Lateral Raise Machine', '상체', 1, NULL, '어깨', 'shoulders', '', '2022-07-27 23:05:55', '2022-07-27 23:07:27', 0),
+	(13, '티 바 로우 머신', 'Tbar-row Machine', '상체', 1, NULL, '광배근', 'lats', '', '2022-07-27 23:06:16', '2022-07-27 23:06:16', 0),
+	(14, '시티드 로우 머신', 'Seated Row Machine', '상체', 1, NULL, '광배근, 승모근', 'lats, traps', '', '2022-07-27 23:06:30', '2022-07-28 00:27:04', 0),
+	(15, '어시스트 풀업 머신', 'Assisted Pullup Machine', '상체', 1, NULL, '광배근', 'lats', '', '2022-07-27 23:06:52', '2022-07-27 23:06:52', 0),
+	(16, '레그 컬 머신', 'Leg Curl Machine', '하체', 1, NULL, '허벅지, 종아리', 'quads, calves', '', '2022-07-27 23:08:13', '2022-07-28 00:22:46', 0),
+	(17, '레그 익스텐션 머신', 'Leg Extension Machine', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:09:36', '2022-07-27 23:10:17', 0),
+	(18, '힙 어덕터', 'Hip Adductor', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:10:07', '2022-07-27 23:12:01', 0),
+	(19, '핵 스쿼트 머신', 'Hack-squat Machine', '하체', 1, NULL, '허벅지, 햄스트링, 둔근', 'quads, hamstrings, glutes', '', '2022-07-27 23:11:59', '2022-07-27 23:11:59', 0),
+	(20, '브이 스쿼트 머신', 'V-squat Machine', '하체', 1, NULL, '허벅지, 둔근', 'quads, glutes', '', '2022-07-27 23:13:58', '2022-07-27 23:14:08', 0),
+	(21, '파워 레그프레스 머신', 'Power Leg Press Machine', '하체', 1, NULL, '허벅지, 둔근', 'quads, glutes', '', '2022-07-27 23:14:36', '2022-07-27 23:14:36', 0),
+	(22, '이너사이 머신', 'Inner Thigh Machine', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:15:02', '2022-07-27 23:15:02', 0),
+	(23, '라잉 레그 컬', 'Lying Leg Curl', '하체', 1, NULL, '허벅지', 'quads', '', '2022-07-27 23:15:26', '2022-07-27 23:15:26', 0),
+	(24, '시티드 레그 프레스 머신', 'Seated Leg Press Machine', '하체', 1, NULL, '허벅지, 엉덩이', 'quads, glutes', '', '2022-07-27 23:15:52', '2022-07-27 23:15:52', 0),
+	(25, '토탈 힙', 'Total Hip machine', '하체', 1, NULL, '허벅지, 엉덩이', 'quads, glutes', '', '2022-07-27 23:16:11', '2022-07-27 23:16:11', 0),
+	(26, '크런치 머신', 'Crunch machine', '상체', 1, NULL, '복근', 'obliques', '', '2022-07-27 23:17:36', '2022-07-27 23:17:36', 0);
 /*!40000 ALTER TABLE `fitness_equipment` ENABLE KEYS */;
 
 -- 테이블 salus.gym 구조 내보내기
