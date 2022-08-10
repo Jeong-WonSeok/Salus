623d7eafff2c9d06e65699d57583bfc18e148bf2
Ubuntu <ubuntu@ip-172-26-13-87.ap-northeast-2.compute.internal>
2022-08-10 08:12:31 +0000

    master: BackEnd Modify

--- BackEnd/socket/socket.js
+++ BackEnd/socket/socket.js
@@ -7,12 +7,22 @@ module.exports = (server) => {
 
     io.on('connection', async (socket) => {
         socket.on('equipmentdata', async (data) => {
-		console.log(data);
+	    console.log(data);
             const test = await exModel.updateIsStarted( {
                 params : { equipmentId : data.equipmentId }
             })
             console.log(test[0][0]);
             socket.emit('equipmentRfidRecieved', test[0][0]);
         })
+	    socket.emit("RFIDcheck", 12341234);
+	//socket.on('RFIDLogin', async (data) => {
+	//	console.log("check:",data);
+	//	io.emit('RFIDcheck', (data));
+	//});
+	//socket.on('RFIDcheck', (data) => {
+	//	console.log(data);
+	//});
+	//socket.emit('RFIDcheck', ('data'));
+		
     });
 }
