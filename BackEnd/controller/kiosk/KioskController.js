87fad774c84db91241f8845ac8151c7c26416194
jiyoung <remi1217@naver.com>
2022-08-11 10:44:55 +0900

    JY-feature: fix error

---
+++ BackEnd/controller/kiosk/KioskController.js
@@ -0,0 +1,11 @@
+const KioskModel = require("../../model/kiosk/KioskModel");
+
+const dailyData = KioskModel.DailyData;
+const userInfo = KioskModel.searchUser;
+const calendarData = KioskModel.calendarData;
+const searchEquipMonthData = KioskModel.equipmentMonthData;
+module.exports = {
+  dailyData,
+  userInfo,
+  searchEquipMonthData,
+};
