7dcb56f53652f2b4bf27988a43c25a889106f728
황상윤 <s_infinite@naver.com>
2022-08-12 14:14:47 +0900

    SY-fix: 프론트 파일경로 수정 및 안쓰는 데이터 더미화

--- FrontEnd/.dummy/src/components/Equipments/MenuTabBar.style.js
+++ FrontEnd/.dummy/src/components/Equipments/MenuTabBar.style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Div } from "../../styles/web.style";
+import { Div } from "../../../../src/styles/web.style";
 export const Label = styled.label`
   display: block;
   height: 100%;
@@ -29,7 +29,6 @@ export const StyledDiv = styled(Div)`
   background: linear-gradient(284.21deg, #92a3fd -7.95%, #9dceff 138.55%);
   border-radius: 5px;
   transition: all 0.35s ease;
-  
 `;
 
 export const NewDiv = styled(Div)`
@@ -75,7 +74,6 @@ export const NewDiv = styled(Div)`
     display: none;
   }
 `;
-
 
 export const Section = styled.section`
   background-color: white;
