6e10e5b4a14963418cbe9d1338e6a6c933a2628c
황상윤 <s_infinite@naver.com>
2022-08-12 14:48:00 +0900

    SY-feature: 캘린더 임시

--- Mobile/components/Calendar/Components/Day.js
+++ Mobile/components/Calendar/Components/Day.js
@@ -1,42 +1,40 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import dayStyles from "../function/styles";
 
-const Day = ({ day, value, onChange }) => {
-  const context = dayStyles(day, value);
+const Day = ({ day, value, onChange, events }) => {
+  const context = dayStyles(day, value, events);
 
   return (
-    <View style={styles.box}>
-      <TouchableOpacity onPress={() => onChange(day)}>
-        <Text
-          style={
-            (context.style === "today" ? styles.today : null) ||
-            (context.style === "selected" ? styles.selected : null) ||
-            (context.style === "other" ? styles.other : null)
-          }
-        >
-          {day.format("D").toString()}
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={() => onChange(day)} style={styles.box}>
+      <Text
+        style={
+          (context.style === "today" ? styles.today : null) ||
+          (context.style === "selected" ? styles.selected : null) ||
+          (context.style === "other" ? styles.other : null) ||
+          (context.style === "event" ? styles.event : null) ||
+          styles.default
+        }
+      >
+        {day.format("D").toString()}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   box: {
     color: "#92a3fd",
-    marginLeft: "5%",
-    padding: 0,
-    margin: 0,
-    marginLeft: 0,
-    fontSize: 40,
-    width: "calc(100% / 7)",
-    // outlineColor: "#dcddde",
-    // outlineStyle: "solid",
-    // outlineWidth: "1px",
-    textAlign: "center",
+    width: Dimensions.get("window").width / 7,
+    narginRight: 2,
     backgroundColor: "white",
-    height: "50px",
+    height: 50,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -45,22 +43,39 @@ const styles = StyleSheet.create({
     backgroundColor: "#92a3fd",
     color: "white",
     textAlign: "center",
-    paddingTop: "2.5px",
-    height: "25px",
-    width: "25px",
-    borderRadius: "50%",
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    overflow: "hidden",
+    fontSize: 18,
   },
   selected: {
     backgroundColor: "#fb816b",
     color: "white",
     textAlign: "center",
-    paddingTop: "2.5px",
-    height: "25px",
-    width: "25px",
-    borderRadius: "50%",
+    // paddingTop: 2.5,
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    overflow: "hidden",
+    fontSize: 18,
   },
   other: {
     color: "lightgray",
+    fontSize: 18,
+  },
+  event: {
+    borderWidth: 1,
+    borderColor: "#fb816b",
+    textAlign: "center",
+    // paddingTop: 2.5,
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    fontSize: 18,
+  },
+  default: {
+    fontSize: 18,
   },
 });
 
