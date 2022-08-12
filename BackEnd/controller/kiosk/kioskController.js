7dcbdf6fca3bb9d55fe32eba03bf19c360c89099
박성배 <sbpark0402@gmail.com>
2022-08-12 10:19:37 +0900

    master: KioskModel.js 삭제

--- BackEnd/controller/kiosk/kioskController.js
+++ BackEnd/controller/kiosk/kioskController.js
@@ -2,7 +2,6 @@ const KioskModel = require("../../model/kiosk/kioskModel");
 
 const dailyData = KioskModel.DailyData;
 const userInfo = KioskModel.searchUser;
-const calendarData = KioskModel.calendarData;
 const searchEquipMonthData = KioskModel.equipmentMonthData;
 module.exports = {
   dailyData,
