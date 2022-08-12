4cc5a71a81c6e759087f772d454f4284ec07956e
wonwoo <lww0407@naver.com>
2022-08-13 02:01:48 +0900

    WW-style

--- Mobile/navigation/Home.js
+++ Mobile/navigation/Home.js
@@ -9,9 +9,6 @@ import {
   Modal,
   TextInput,
   TouchableOpacity,
-  Dimensions,
-  ActivityIndicator,
-  Animated,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Container } from '../theme/global-theme';
@@ -21,28 +18,84 @@ import { LChart, PChart } from '../components/Chart/Chart';
 import ExerciseList from '../components/MainExercise/ExerciseList';
 import TimeScroll from '../components/TimeScroll/TimeScroll';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { Fragment, useState, useCallback, useEffect } from 'react';
+import React, { useState } from 'react';
 import useHttp from '../hooks/useHttp';
+
 
 const Home = ({ navigation }) => {
   const { apiRequest } = useHttp();
   const [loading, setLoading] = useState(true);
-  const [thisweek, setThisWeek] = useState();
-  const [userId, setUserId] = useState();
+  const [data, setData] = useState([
+    [
+      {
+        excerciseDay: '22-08-08',
+        totalVolume: '4200',
+      },
+      {
+        excerciseDay: '22-08-09',
+        totalVolume: '5250',
+      },
+      {
+        excerciseDay: '22-08-10',
+        totalVolume: '9850',
+      },
+      {
+        excerciseDay: '22-08-11',
+        totalVolume: '5300',
+      },
+    ],
+    [
+      {
+        targetTime: '16:00',
+        targetVolume: 60000,
+      },
+    ],
+    [
+      {
+        percentTime: '37',
+        percentVolume: '41',
+      },
+    ],
+    [
+      {
+        equipmentName: '레그 컬 머신',
+        totalCount: '50',
+        totalVolume: '3200',
+        carorie: '143',
+        totalTime: '20',
+      },
+      {
+        equipmentName: '숄더프레스 머신',
+        totalCount: '20',
+        totalVolume: '2100',
+        carorie: '150',
+        totalTime: '21',
+      },
+    ],
+  ]);
+  // const user_id = AsyncStorage.getItem('user_id');
+  const user_id = '977231111725';
+
+  const thisweek = [26, 63, 7, 68, 50, 37, 100];
+  const timePercent = Number('0.' + data[2][0].percentTime);
+  const volumePercent = Number('0.' + data[2][0].percentVolume);
+  const exerciseData = data[3][0];
+  const targetHour = Number(data[1][0].targetTime.slice(0, 2));
+  const targetMinute = Number(data[1][0].targetTime.slice(3));
+  const targetVolume = data[1][0].targetVolume;
+
   const [timeModal, setTimeModal] = useState(false);
   const [volumeModal, setVolumeModal] = useState(false);
 
-  const [percentTime, setPercentTime] = useState();
-  const [percentVolume, setPercentVolume] = useState();
-  const [exercise, setExercise] = useState();
   // 완료 누르면 설정되는 목표 (데이터에 저장됨)
-  const [myHour, setMyHour] = useState('0');
-  const [myMinute, setMyMinute] = useState('0');
-  const [myVolume, setMyVolume] = useState();
+  const [myHour, setMyHour] = useState(targetHour);
+  const [myMinute, setMyMinute] = useState(targetMinute);
+  const [myVolume, setMyVolume] = useState(targetVolume);
+
   // 유저가 입력한 목표 (취소 누르면 날아감, 데이터에 저장 X)
-  const [nowHour, setNowHour] = useState('0');
-  const [nowMinute, setNowMinute] = useState('0');
-  const [nowVolume, setNowVolume] = useState();
+  const [nowHour, setNowHour] = useState(targetHour);
+  const [nowMinute, setNowMinute] = useState(targetMinute);
+  const [nowVolume, setNowVolume] = useState(targetVolume);
 
   const hourHandler = (data) => {
     setNowHour(data);
@@ -50,79 +103,48 @@ const Home = ({ navigation }) => {
   const minuteHandler = (data) => {
     setNowMinute(data);
   };
-  const thisWeekHandler = (data) => {
-    const weekList = [0, 0, 0, 0, 0, 0, 0];
-    const days = [0, 1, 2, 3, 4, 5, 6];
-    check = () => {
-      for (const day of days) {
-        for (const record of data[0]) {
-          if (moment().startOf('week').add(day, 'day').format('YY-MM-DD') === record.excerciseDay) {
-            weekList[day] = record.totalVolume;
-          }
-        }
-      }
-    };
-    check();
-    return weekList;
-  };
-  const getData = useCallback((res) => {
-    console.log(res);
-    setThisWeek(thisWeekHandler(res));
-    setMyHour(Number(res[1][0].targetTime.slice(0, 2)));
-    setMyMinute(Number(res[1][0].targetTime.slice(3)));
-    setMyVolume(Number(res[1][0].targetVolume));
-    setPercentTime(Number(res[2][0].percentTime) * 0.01);
-    setPercentVolume(Number(res[2][0].percentVolume) * 0.01);
-    setExercise(res[3]);
-    setLoading(false);
-    console.log(loading);
-  }, []);
 
-  // 페이지 렌더시 첫 데이터 받아오기
-  useEffect(() => {
-    AsyncStorage.getItem('@user_id').then((value) => {
-      console.log(value);
-      setUserId(value);
-    });
-    console.log(userId);
-    apiRequest(
-      {
-        url: `http://i7b110.p.ssafy.io:3010/mobile/user/${userId}`,
-      },
-      getData
-    );
-  }, [apiRequest, getData, userId]);
+  // const getData = useCallback((data) => {
+  //   setData(data);
+  //   setLoading(false);
+  // }, []);
+
+  //데이터 받아오기
+  // useEffect(() => {
+  //   apiRequest(
+  //     {
+  //       url: `http://i7b110.p.ssafy.io:3010/mobile/user${user_id}`,
+  //     },
+  //     getData
+  //   );
+  // }, [apiRequest, getData, myHour, myMinute, myVolume]);
 
   const timeData = {
-    rfidKey: userId,
+    rfidKEy: user_id,
     targetTime:
-      (String(nowHour).length == 1 ? '0' + nowHour : nowHour) +
+      (String(myHour).length == 1 ? '0' + myHour : myHour) +
       ':' +
-      (String(nowMinute).length == 1 ? '0' + nowMinute : nowMinute) +
+      (String(myMinute).length == 1 ? '0' + myMinute : myMinute) +
       ':' +
       '00',
   };
   const volumeData = {
-    rfidKey: userId,
-    targetVolume: nowVolume,
+    rfidKEy: user_id,
+    targetVolume: myVolume,
   };
 
   // 목표 시간 설정
-  const TimeFunc = async () => {
-    setTimeModal(false);
-    axios({
-      url: 'http://i7b110.p.ssafy.io:3010/mobile/updateTime',
-      method: 'post',
-      data: timeData,
+  const TimeFunc = () => {
+    fetch('http://i7b110.p.ssafy.io:3010/mobile/updateTime', {
+      method: 'POST',
+      body: JSON.stringify(timeData),
     })
-      .then(() => {
-        console.log('time');
-        apiRequest(
-          {
-            url: `http://i7b110.p.ssafy.io:3010/mobile/user/${userId}`,
-          },
-          getData
-        );
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        if (responseJson) {
+          setTimeModal(!timeModal);
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -130,185 +152,155 @@ const Home = ({ navigation }) => {
   };
 
   // 목표 볼륨 설정
-  const VolumeFunc = async () => {
-    axios({
-      url: 'http://i7b110.p.ssafy.io:3010/mobile/updateVolume',
+  const VolumeFunc = () => {
+    fetch('http://i7b110.p.ssafy.io:3010/mobile/updateVolume', {
       method: 'POST',
-      data: volumeData,
+      body: JSON.stringify(volumeData),
     })
-      .then(() => {
-        console.log('volume');
-        apiRequest(
-          {
-            url: `http://i7b110.p.ssafy.io:3010/mobile/user/${userId}`,
-          },
-          getData
-        );
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        if (responseJson) {
+          setTimeModal(!volumeModal);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const LogoutFunc = () => {
-    AsyncStorage.removeItem('@user_id');
-    navigation.navigate('SplashScreen');
-  };
 
   return (
-    <Fragment>
-      {!loading ? (
-        <ScrollView style={styles.scrollview}>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={timeModal}
-            onRequestClose={() => {
-              Alert.alert('Modal has been closed.');
-              setTimeModal(!timeModal);
-            }}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>이번주 목표 시간 설정</Text>
-                <TimeScroll onHour={hourHandler} onMinute={minuteHandler} myHour={myHour} myMinute={myMinute} />
-                <View style={styles.modal}>
-                  <Pressable style={styles.buttonCancle} onPress={() => setTimeModal(!timeModal)}>
-                    <Text style={styles.textStyle}>취소</Text>
-                  </Pressable>
-                  <Pressable
-                    style={styles.buttonSubmit}
-                    onPress={() => {
-                      TimeFunc();
-                    }}
-                  >
-                    <Text style={styles.textStyle}>완료</Text>
-                  </Pressable>
-                </View>
-              </View>
+    <ScrollView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={timeModal}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setTimeModal(!timeModal);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>이번주 목표 시간 설정</Text>
+            <TimeScroll onHour={hourHandler} onMinute={minuteHandler} myHour={myHour} myMinute={myMinute} />
+            <View style={styles.modal}>
+              <Pressable style={styles.buttonCancle} onPress={() => setTimeModal(!timeModal)}>
+                <Text style={styles.textStyle}>취소</Text>
+              </Pressable>
+              <Pressable
+                style={styles.buttonSubmit}
+                onPress={() => {
+                  setMyHour(nowHour);
+                  setMyMinute(nowMinute);
+                  TimeFunc;
+                }}
+              >
+                <Text style={styles.textStyle}>완료</Text>
+              </Pressable>
             </View>
-          </Modal>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={volumeModal}
-            onRequestClose={() => {
-              Alert.alert('Modal has been closed.');
-              setVolumeModal(!volumeModal);
-            }}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalViewVolume}>
-                <Text style={styles.modalText}>이번주 목표 볼륨 설정</Text>
-                <View style={styles.modal}>
-                  <TextInput
-                    style={styles.input}
-                    onChangeText={(text) => {
-                      setNowVolume(Number(text));
-                    }}
-                    defaultValue={String(myVolume)}
-                    keyboardType="numeric"
-                    autoFocus={true}
-                  />
-                  <Text style={styles.unit}>Kg</Text>
-                </View>
-                <View style={styles.modal}>
-                  <Pressable style={styles.buttonCancle} onPress={() => setVolumeModal(!volumeModal)}>
-                    <Text style={styles.textStyle}>취소</Text>
-                  </Pressable>
-                  <Pressable
-                    style={styles.buttonSubmit}
-                    onPress={() => {
-                      setVolumeModal(!volumeModal);
-                      VolumeFunc();
-                    }}
-                  >
-                    <Text style={styles.textStyle}>완료</Text>
-                  </Pressable>
-                </View>
-              </View>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={volumeModal}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setVolumeModal(!volumeModal);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalViewVolume}>
+            <Text style={styles.modalText}>이번주 목표 볼륨 설정</Text>
+            <View style={styles.modal}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(text) => setNowVolume(Number(text))}
+                defaultValue={String(myVolume)}
+                keyboardType="numeric"
+                autoFocus={true}
+              />
+              <Text style={styles.unit}>Kg</Text>
             </View>
-          </Modal>
-          <Container flexDirection="column">
-            <Container flex={1} justifyContent="space-between" mt={40} mb={10}>
-              <Text style={styles.logo}>Salus</Text>
-              <View>
-                <TouchableOpacity
-                  onPress={() => {
-                    navigation.navigate('Calendar');
-                  }}
-                >
-                  <Image source={calendar} style={styles.image} />
-                </TouchableOpacity>
-                <Text style={styles.calendartext}>캘린더</Text>
-              </View>
+            <View style={styles.modal}>
+              <Pressable style={styles.buttonCancle} onPress={() => setVolumeModal(!volumeModal)}>
+                <Text style={styles.textStyle}>취소</Text>
+              </Pressable>
+              <Pressable
+                style={styles.buttonSubmit}
+                onPress={() => {
+                  setMyVolume(nowVolume);
+                  setVolumeModal(!volumeModal);
+                  VolumeFunc;
+                }}
+              >
+                <Text style={styles.textStyle}>완료</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Container flexDirection="column">
+        <Container flex={1} justifyContent="space-between" mt={40} mb={10}>
+          <Text style={styles.logo}>Salus</Text>
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Calendar');
+              }}
+            >
+              <Image source={calendar} style={styles.image} />
+            </TouchableOpacity>
+            <Text style={styles.calendartext}>캘린더</Text>
+          </View>
+        </Container>
+        <Container flex={9} flexDirection="column">
+          <Container flex={6} flexDirection="column">
+            <Container justifyContent="space-between">
+              <Text style={styles.exercise}>이번주 운동(볼륨)</Text>
+              <Text style={styles.week}>{todayFormal()}</Text>
             </Container>
-            <Container flex={9} flexDirection="column">
-              <Container flex={6} flexDirection="column">
-                <Container justifyContent="space-between">
-                  <Text style={styles.exercise}>이번주 운동(볼륨)</Text>
-                  <Text style={styles.week}>{todayFormal()}</Text>
+            <View>
+              <LChart data={thisweek} />
+            </View>
+          </Container>
+          <Container flex={3} flexDirection="column" mt={10} mb={10}>
+            <Text style={styles.exercise}>이번주 목표</Text>
+            <Container justifyContent="space-around">
+              <Pressable onPress={() => setTimeModal(true)}>
+                <Container flexDirection="column">
+                  <PChart data={timePercent} />
+                  <Text style={styles.goal}>시간</Text>
                 </Container>
-                <View>
-                  <LChart data={thisweek} />
-                </View>
-              </Container>
-              <Container flex={3} flexDirection="column" mt={10} mb={10}>
-                <Text style={styles.exercise}>이번주 목표</Text>
-                <Container justifyContent="space-around">
-                  <Pressable onPress={() => setTimeModal(true)}>
-                    <Container flexDirection="column">
-                      <PChart data={percentTime} />
-                      <Text style={styles.goal}>시간</Text>
-                    </Container>
-                  </Pressable>
-                  <Pressable onPress={() => setVolumeModal(true)}>
-                    <Container flexDirection="column">
-                      <PChart data={percentVolume} />
-                      <Text style={styles.goal}>볼륨</Text>
-                    </Container>
-                  </Pressable>
+              </Pressable>
+              <Pressable onPress={() => setVolumeModal(true)}>
+                <Container flexDirection="column">
+                  <PChart data={volumePercent} />
+                  <Text style={styles.goal}>볼륨</Text>
                 </Container>
-              </Container>
-              <Container flex={5} flexDirection="column">
-                <Container mb={5}>
-                  <Text style={styles.exercise}>오늘 완료한 운동</Text>
-                </Container>
-                <Container>
-                  <ExerciseList data={exercise} />
-                </Container>
-              </Container>
-              <Button title="go to CurrentExercise" onPress={() => navigation.navigate('Exercise')} />
-              <Pressable onPressIn={fadeIn} onPressOut={fadeOut} onPress={LogoutFunc}>
-                <Animated.View style={styles.button}>
-                  <Text style={styles.text}>로그아웃</Text>
-                </Animated.View>
               </Pressable>
             </Container>
-            <StatusBar style="dark" />
           </Container>
-        </ScrollView>
-      ) : (
-        <Container flexDirection="column">
-          <LinearGradient colors={['#92a3fd', '#9dceff']} style={styles.background} />
-          <Image source={logo} style={styles.loadinglogo} />
-          <Text style={styles.loadingtext}>운동기록 로딩중...</Text>
-          <ActivityIndicator animating={true} color="white" size="large" style={styles.activityIndicator} />
+          <Container flex={5} flexDirection="column">
+            <Container mb={5}>
+              <Text style={styles.exercise}>오늘 완료한 운동</Text>
+            </Container>
+            <Container>
+              <ExerciseList data={exerciseData} />
+            </Container>
+          </Container>
+          <Button title="go to CurrentExercise" onPress={() => navigation.navigate('Exercise')} />
         </Container>
-      )}
-    </Fragment>
+        <StatusBar style="dark" />
+      </Container>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollview: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: screenHeight,
-    backgroundColor: 'white',
-  },
   logo: {
     color: '#92a3fd',
     marginLeft: '5%',
@@ -416,40 +408,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     lineHeight: 65,
     color: '#rgba(99, 126, 255, 0.5)',
-  },
-  loadinglogo: {
-    width: screenWidth * 0.8,
-    height: (screenWidth * 0.8) / 2.6,
-    marginBottom: 50,
-  },
-  activityIndicator: {
-    alignItems: 'center',
-    height: 80,
-  },
-  background: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: screenHeight,
-  },
-  loadingtext: {
-    color: 'white',
-  },
-  button: {
-    opacity: animated,
-    width: screenWidth * 0.6,
-    height: 48,
-    padding: 15,
-    alignItems: 'center',
-    borderRadius: 30,
-    marginTop: 50,
-    backgroundColor: '#7a91ff',
-  },
-  text: {
-    backgroundColor: 'transparent',
-    fontSize: 18,
-    color: '#fff',
   },
 });
 
