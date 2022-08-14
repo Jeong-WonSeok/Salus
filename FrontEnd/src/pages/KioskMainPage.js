80e70edd372d44e4021fb2e3ddf48bf51559dada
jiyoung <remi1217@naver.com>
2022-08-14 16:37:31 +0900

    JY-feature: kiosk main page 운동리스트 css 수정

--- FrontEnd/src/pages/KioskMainPage.js
+++ FrontEnd/src/pages/KioskMainPage.js
@@ -1,24 +1,24 @@
-import React, { Fragment, useState } from "react";
-import { CustomDiv } from "../styles/kiosk.style";
-import StatsWeekly from "../components/Kiosk/StatsWeekly";
-import StatsMuscle from "./../components/Kiosk/StatsMuscle";
-import NavKiosk from "./../components/Kiosk/NavKiosk";
-import SelectBoardView from "../components/Kiosk/SelectBoardView";
-import WeeklySummary from "./../components/Kiosk/WeeklySummary";
-import Exercise from "../components/Kiosk/Exercise";
-import { createGlobalStyle } from "styled-components";
-import { Div } from "./../styles/kiosk.style";
-import CalendarForm from "./../components/Calendar/CalendarForm";
-import moment from "moment";
-import ButtonComponent from "./../components/Common/ButtonComponent";
-import setDailyDatas from "../components/Kiosk/KioskFuntions/setDailyDatas";
-import styled from "styled-components";
-import useHttp from "./../customHooks/useHttp";
-import { useEffect } from "react";
-import { useCallback } from "react";
-import { ChartCalc } from "./../api-request/functions";
-import LoadingText from "./../components/Kiosk/LoadingText";
-import { useNavigate } from "react-router-dom";
+import React, { Fragment, useState } from 'react';
+import { CustomDiv } from '../styles/kiosk.style';
+import StatsWeekly from '../components/Kiosk/StatsWeekly';
+import StatsMuscle from './../components/Kiosk/StatsMuscle';
+import NavKiosk from './../components/Kiosk/NavKiosk';
+import SelectBoardView from '../components/Kiosk/SelectBoardView';
+import WeeklySummary from './../components/Kiosk/WeeklySummary';
+import Exercise from '../components/Kiosk/Exercise';
+import { createGlobalStyle } from 'styled-components';
+import { Div } from './../styles/kiosk.style';
+import CalendarForm from './../components/Calendar/CalendarForm';
+import moment from 'moment';
+import ButtonComponent from './../components/Common/ButtonComponent';
+import setDailyDatas from '../components/Kiosk/KioskFuntions/setDailyDatas';
+import styled from 'styled-components';
+import useHttp from './../customHooks/useHttp';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { ChartCalc } from './../api-request/functions';
+import LoadingText from './../components/Kiosk/LoadingText';
+import { useNavigate } from 'react-router-dom';
 
 const HomeDiv = styled(Div)`
   position: absolute;
@@ -36,13 +36,15 @@ const KioskMainPage = () => {
   const { apiRequest } = useHttp();
   const [equipmentData, setEquipmentData] = useState();
   const [isBoard, setIsBoard] = useState(true);
-  const [selectedDate, setSelectedDate] = useState(moment().format("YY-MM-DD"));
+  const [selectedDate, setSelectedDate] = useState(moment().format('YY-MM-DD'));
   const [value, setValue] = useState(moment());
   const [loading, setLoading] = useState(true);
+  const [back, setBack] = useState(false);
   const navigate = useNavigate();
   const removeRFID = () => {
-    localStorage.removeItem("RFID");
-    navigate("/kiosk/login");
+    localStorage.removeItem('RFID');
+    navigate('/kiosk/login');
+    setBack(false);
   };
 
   const getEquipmentData = useCallback((data) => {
@@ -53,14 +55,14 @@ const KioskMainPage = () => {
   useEffect(() => {
     apiRequest(
       {
-<<<<<<< HEAD
+        // url: `http://i7b110.p.ssafy.io:3010/kiosk/login/${parseInt(JSON.parse(localStorage.getItem('RFID'))?.rfidKey)}`,
         url: `http://i7b110.p.ssafy.io:3010/kiosk/login/977237223725`,
-=======
-        url: `http://i7b110.p.ssafy.io:3010/kiosk/login/${parseInt(JSON.parse(localStorage.getItem("RFID"))?.rfidKey)}`,
->>>>>>> 80500c92483e447b6a48569a3ce1903f6badd859
       },
       getEquipmentData
     );
+    setTimeout(() => {
+      setBack(true);
+    }, 7000);
   }, [apiRequest, getEquipmentData]);
 
   // Boad / Calender 선택
@@ -81,63 +83,35 @@ const KioskMainPage = () => {
         <Fragment>
           <CustomDiv>
             <NavKiosk />
-            <CustomDiv
-              displayDiv="flex"
-              mt={20}
-              mr={30}
-              ml={30}
-              borderRadius="10px"
-              justifyContent="space-between"
-            >
+            <CustomDiv displayDiv="flex" mt={20} mr={30} ml={30} borderRadius="10px" justifyContent="space-between">
               <StatsMuscle
                 muscle={setDailyDatas(equipmentData[0], selectedDate)[0]}
                 date={selectedDate}
                 gender={parseInt(equipmentData[0][0].gender)}
               />
-              <StatsWeekly
-                ratioData={ChartCalc(
-                  equipmentData[1],
-                  equipmentData[2],
-                  equipmentData[3][0]
-                )}
-              />
+              <StatsWeekly ratioData={ChartCalc(equipmentData[1], equipmentData[2], equipmentData[3][0])} />
             </CustomDiv>
             <SelectBoardView onSelectData={saveBoadHandler} />
             {isBoard ? (
               <Div mt={10}>
-                <WeeklySummary
-                  datas={equipmentData[0]}
-                  onChange={getData}
-                  date={selectedDate}
-                />
+                <WeeklySummary datas={equipmentData[0]} onChange={getData} date={selectedDate} />
                 <Exercise
-                  exerciseList={
-                    setDailyDatas(equipmentData[0], selectedDate)[1]
-                  }
+                  exerciseList={setDailyDatas(equipmentData[0], selectedDate)[1]}
                   chartData={equipmentData[4]}
                 />
               </Div>
             ) : (
               <Div mt={10}>
-                <CalendarForm
-                  events={equipmentData[5]}
-                  value={value}
-                  onChange={setValue}
-                />
+                <CalendarForm events={equipmentData[5]} value={value} onChange={setValue} />
               </Div>
             )}
           </CustomDiv>
           <HomeDiv displayDiv="flex" justifyContent="center">
-            <ButtonComponent
-              buttonText="홈"
-              buttonWidth="35vw"
-              buttonHeight="3vh"
-              onClick={removeRFID}
-            />
+            <ButtonComponent buttonText="홈" buttonWidth="35vw" buttonHeight="3vh" onClick={removeRFID} />
           </HomeDiv>
         </Fragment>
       ) : (
-        <LoadingText />
+        <LoadingText>{back ? removeRFID() : null}</LoadingText>
       )}
     </Fragment>
   );
